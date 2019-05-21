6c0359f3032a2d40594c5615b2abeb735724f833
tolga.kamci <tolga.kamci@BK-LAB204-IS020>
2019-05-21 15:02:56 +0300

    login ekranı ve anasayfa ekranı tasarlandı.

---
+++ EmployeeLogin/EmployeeLogin.UI.Web/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeLogin.UI.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeLogin.UI.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult LoginCRM(LoginModel model)
+        {
+            //var employee = AccessBLL.LoginUser(model.UserName, model.Password);
+
+            //if (employee == null)
+            //{
+            //    TempData["Hata"] = "Kullanıcı bulunamadı";
+            //    return RedirectToAction("Index");
+            //}
+            //else
+            //{
+            //    Session["Employee"] = employee;
+            //    return RedirectToAction("Index", "Home");
+            //}
+            return RedirectToAction("Homepage","Home");
+
+        }
+    }
+}
---
+++ EmployeeLogin/EmployeeLogin.UI.Web/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EmployeeLogin.UI.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        protected void Application_Start()
+        {
+            log.Info("Ya Hak!");
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
