19313b596498f9b8dd30de86eb853c123f504989
Administrator <Administrator@DESKTOP-O269UOM>
2019-05-22 15:14:33 +0300

    Değ.22.05.2019

--- EmployeeLogin/EmployeeLogin/Controllers/OrderController.cs
+++ EmployeeLogin/EmployeeLogin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeLogin.BLL;
 using EmployeeLogin.DAL;
+using EmployeeLogin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,11 @@ namespace EmployeeLogin.Controllers
 {
     public class OrderController : Controller
     {
+        [MyAuthenticationFilter]
         // GET: Order
         public ActionResult Index()
         {
-            if (Session["Employee"] == null)
-            {
-                return RedirectToAction("Hata", "Home");
-            }
+           
 
             var employee = Session["Employee"] as Employee;
 
